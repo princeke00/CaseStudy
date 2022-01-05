6dff7b55ba37432cb0f983861028ff31c6ae938e
princeke00 <princeke00@gmail.com>
2022-01-05 19:17:38 +0530

    final

---
+++ API-Gateway/src/main/java/com/gateway/api/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.gateway.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
